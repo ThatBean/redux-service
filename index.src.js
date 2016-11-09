5c3d310b88038161b16a9350389e8e424483730f
ThatBean <Goomailbean@gmail.com>
2016-11-09 16:39:37 +0800

    link GitBook Document & code clear up
    * link GitBook Document
    * code clear up

--- index.src.js
+++ index.src.js
@@ -13,35 +13,28 @@
  */
 
 const DEFAULT_RESPOND = { done: true, value: null }
+const TO_BOOL_MAP = (sourceList) => [].concat(sourceList).reduce((o, v) => { o[ v ] = true; return o }, {})
 
 class LiteCSP {
-  constructor (type) {
-    this.type = type
+  constructor (name) {
+    this.name = name
     this.generator = null
     this.observer = null // a observer Function
-    this.running = false
-    this.isEnteredInput = false
-    this.expect = {} // key - true
+    this.isActive = false
+    this.requestMap = {} // Map: { actionType: true }
     this.respond = DEFAULT_RESPOND
-
+    this.inputAction = null
     this.contextObject = {
-      req: (keyList) => { // console.log('- REQ', this.type, keyList)
-        this.expect = [].concat(keyList).reduce((o, v) => {
-          o[ v ] = true
-          return o
-        }, {})
-      },
-      res: (data) => { // console.log('- RES', this.type, data.type)
-        this.respond = { done: false, value: data }
-      }
+      req: (actionTypeList) => { this.requestMap = TO_BOOL_MAP(actionTypeList) },
+      res: (action) => { this.respond = { done: false, value: action } }
     }
   }
 
   linkGenerator (generatorFunction, contextObject) {
     this.generator = generatorFunction({ ...this.contextObject, ...contextObject })
-    this.generatorNext = (data) => {
+    this.generatorNext = (action) => {
       this.respond = DEFAULT_RESPOND
-      if (this.running) this.running = (this.generator.next(data).done === false)
+      if (this.isActive) this.isActive = (this.generator.next(action).done === false)
     }
   }
 
@@ -49,39 +42,34 @@ class LiteCSP {
     this.observer = observer
   }
 
-  start (data) {
-    if (this.running) return
-    this.running = true
-    this.next(data)
+  start (action) {
+    if (this.isActive) return
+    this.isActive = true
+    this.next(action)
   }
 
-  input (key, data) {
-    let isBlock = false
-    if (this.running && this.expect[ key ]) {
-      if (this.isEnteredInput) console.error(`[ReduxService][LiteCSP] unexpected re-entry of ${this.type}, key: ${key}`)
-      else this.isEnteredInput = true
-      this.next(data)
-      this.isEnteredInput = false
-      isBlock = true
-    }
-    return isBlock
+  input (action) {
+    if (!this.isActive || !this.requestMap[ action.type ]) return false
+    if (this.inputAction) console.warn(`[ReduxService] re-entry of [${this.name}], already with: ${this.inputAction.type}, new action: ${action.type}`)
+    this.inputAction = action
+    this.next(action)
+    this.inputAction = null
+    return true // Block Action
   }
 
-  next (data) {
-    // console.log('>>>> NEXT', this.type, data)
-    this.generatorNext(data)
-    while (this.observer && this.running && !this.respond.done) { // process follow up res by calling next till a req
+  next (action) {
+    this.generatorNext(action)
+    while (this.observer && this.isActive && !this.respond.done) { // process follow up res by calling next till a req
       this.observer(this.respond.value)
-      !this.respond.done && this.generatorNext(data)
+      !this.respond.done && this.generatorNext(action)
     }
-    // console.log('>>>> NEXT', this.type, data)
   }
 
-  stop (data) {
-    if (!this.running) return
-    this.generator.return(data)
-    this.running = false
-    this.expect = {}
+  stop (action) {
+    if (!this.isActive) return
+    this.generator.return(action)
+    this.isActive = false
+    this.requestMap = {}
   }
 }
 
@@ -108,82 +96,75 @@ class ReduxService {
     this.entryMap = {} // actionType - serviceEntryFunction
     this.serviceMap = {} // serviceType - LiteCSP
     this.serviceGeneratorFunctionMap = {}
-
-    // bind method to this[key] & this.bindMap[key]
-    this.bindMap = {}
+    this.bindMap = {} // bind method to this[key] & this.bindMap[key]
     BIND_KEY_LIST.forEach((key) => ( this[ key ] = this.bindMap[ key ] = this[ key ].bind(this) ))
   }
 
   middleware (store) {
-    this.setStore(store)
-    return (next) => action => this.onAction(action) || next(action) // pick this action from the reducer
+    this.store = store
+    return (next) => action => this.onAction(action) || next(action) // if Action Block, skip next()
   }
 
-  setStore (store) { this.store = store }
-
-  setEntry (type, entryFunction) {
-    if (this.entryMap[ type ]) console.warn('[ReduxService] possible unexpected entry overwrite:', type, entryFunction)
-    this.entryMap[ type ] = entryFunction
+  setEntry (actionType, entryFunction) {
+    if (this.entryMap[ actionType ]) console.warn('[ReduxService] possible unexpected entry overwrite:', actionType, entryFunction)
+    this.entryMap[ actionType ] = entryFunction
   }
 
-  setService (type, serviceGeneratorFunction) {
-    if (this.serviceGeneratorFunctionMap[ type ]) console.warn('[ReduxService] possible unexpected service overwrite:', type, serviceGeneratorFunction)
-    this.serviceGeneratorFunctionMap[ type ] = serviceGeneratorFunction
+  setService (serviceName, serviceGeneratorFunction) {
+    if (this.serviceGeneratorFunctionMap[ serviceName ]) console.warn('[ReduxService] possible unexpected service overwrite:', serviceName, serviceGeneratorFunction)
+    this.serviceGeneratorFunctionMap[ serviceName ] = serviceGeneratorFunction
   }
 
-  startService (type) {
-    const serviceGeneratorFunction = this.serviceGeneratorFunctionMap[ type ]
-    if (!serviceGeneratorFunction) return console.warn('[ReduxService] service not found:', type)
-    if (this.serviceMap[ type ]) return console.warn('[ReduxService] service already started:', type)
-    const service = new LiteCSP(type)
-    service.linkGenerator(serviceGeneratorFunction, {
-      store: this.store,
-      ...this.bindMap
-    })
-    service.linkObserver((...args) => { // console.log('linkObserver', ...args)
-      return this.store.dispatch(...args)
-    })
+  startService (serviceName) {
+    const serviceGeneratorFunction = this.serviceGeneratorFunctionMap[ serviceName ]
+    if (!serviceGeneratorFunction) return console.warn('[ReduxService] service not found:', serviceName)
+    if (this.serviceMap[ serviceName ]) return console.warn('[ReduxService] service already started:', serviceName)
+    const service = new LiteCSP(serviceName)
+    service.linkGenerator(serviceGeneratorFunction, { store: this.store, ...this.bindMap })
+    service.linkObserver((action) => this.store.dispatch(action))
     service.start()
-    if (!service.running) return console.warn('[ReduxService] service failed to start:', type)
-    this.serviceMap[ type ] = service
+    if (!service.isActive) return console.warn('[ReduxService] service failed to start:', serviceName)
+    this.serviceMap[ serviceName ] = service
   }
 
   startAllService () {
-    for (const type in this.serviceGeneratorFunctionMap) !this.serviceMap[ type ] && this.startService(type)
+    for (const serviceName in this.serviceGeneratorFunctionMap) {
+      if (!this.serviceMap[ serviceName ]) this.startService(serviceName)
+    }
   }
 
-  stopService (type) {
-    const service = this.serviceMap[ type ]
+  stopService (serviceName, action) {
+    const service = this.serviceMap[ serviceName ]
     if (!service) return
-    service.stop()
-    delete this.serviceMap[ service.type ]
+    service.stop(action)
+    delete this.serviceMap[ serviceName ]
   }
 
   onAction (action) {
-    if (!this.store) console.warn('[ReduxService] caught action before store configured:', action)
+    if (!this.store) return console.warn(`[ReduxService] get Action before Store is set, strange. Action Type: ${action.type}`)
 
     const entry = this.entryMap[ action.type ]
     if (entry) { // console.log('[ReduxService] Entry:', action.type)
       const isBlock = entry(this.store, action)
-      if (isBlock) return true // if the entry return true, follow up middleware & reducers will be blocked
+      if (isBlock) return true // if isBlock, follow up service || middleware || reducers will not receive this Action
     }
 
-    for (const type in this.serviceMap) {
-      const service = this.serviceMap[ type ]
-      if (service.input(action.type, action)) {
-        // !service.running && console.log('[ReduxService] service stopped:', service.type)
-        if (!service.running) delete this.serviceMap[ service.type ]
-        return true // always block
+    for (const serviceName in this.serviceMap) {
+      const service = this.serviceMap[ serviceName ]
+      if (service.input(action)) {
+        // !service.isActive && console.log('[ReduxService] service stopped:', service.name)
+        if (!service.isActive) delete this.serviceMap[ service.name ]
+        return true // always Block Action here
       }
     }
 
-    return false // if the entry return true, follow up middleware & reducers will be blocked
+    return false // Action not Blocked
   }
 }
 
 // the session Object Appears to be 'Immutable', but not necessarily the Array or Object inside
-function createSessionReducer (actionType, session) {
-  const initialState = { ...session, _tick: 0 }
+function createSessionReducer (actionType, sessionObject) {
+  const initialState = { ...sessionObject, _tick: 0 }
   return (state = initialState, action) => {
     if (action.type === actionType) return { ...state, ...action.payload, _tick: state._tick + 1 }
     else return state
@@ -191,11 +172,8 @@ function createSessionReducer (actionType, session) {
 }
 
 export {
-  ReduxService, // for manually create new instance
+  ReduxService, // for multi instance
   createSessionReducer
 }
 
-export default {
-  ReduxService, // for manually create new instance
-  createSessionReducer
-}
+export default ReduxService
